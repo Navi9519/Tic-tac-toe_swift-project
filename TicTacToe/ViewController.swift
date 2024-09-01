f43a3bc8d32a264c89a7129111b2d2bee2831747
Navi9519 <dedicivanovic@gmail.com>
2024-09-01 20:14:39 +0200

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Ivan Dedic on 2024-09-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
