843566abb8d6f649af98a6d1848f9f272a88af63
Navi9519 <dedicivanovic@gmail.com>
2024-09-09 18:21:01 +0200

    created a handler function for both X and O drag to keep logic within only one function

--- TicTacToe/Controller/GameViewController.swift
+++ TicTacToe/Controller/GameViewController.swift
@@ -9,22 +9,22 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-
+    
     
     // objects
     
     var game = Game()
     var PLAYER_ONE = Player(name: "Ivan", id: 1)
     var PLAYER_TWO = Player(name: "Onur", id: 2)
-  
+    
     // variables
     
-    var isPlaying: Bool = true
-   
-        
+    var playerOneIsPlaying: Bool = true
+    
+    
     
     /// Outlets ///
-   
+    
     @IBOutlet var Squares: [UIImageView]!
     @IBOutlet weak var gameBoardStack: UIStackView!
     
@@ -38,174 +38,138 @@ class GameViewController: UIViewController {
     
     var initialSymbolPosition: CGPoint = CGPoint.zero
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         lblPlayerOne.text = "Player 1s turn"
         lblPlayerTwo.text = "Player 2"
         
         initialSymbolPosition = xSymbol.center
         initialSymbolPosition = oSymbol.center
-
+        
         
         
     }
     
     
     func resetGame() {
+        
         game.resetBoard()
+        
         for square in Squares {
             square.image = UIImage(systemName: "square")
             square.tintColor = UIColor.black
         }
         
+        playerOneIsPlaying = true
         lblPlayerOne.text = "Player 1s turn"
         lblPlayerTwo.text = "Player 2"
-        isPlaying.toggle()
     }
     
-
-
     
-    // Can I do collection of these two?
+    
+    
+    
     
     @IBAction func onXDrag(_ sender: UIPanGestureRecognizer) {
-         
-        if(isPlaying) {
-            
-            
         
         
-        let translation = sender.translation(in: self.view)
-         
-        xSymbol.center = CGPoint(x: translation.x + xSymbol.center.x, y: translation.y + xSymbol.center.y )
-        
-        sender.setTranslation(CGPoint.zero, in: self.view)
-        
-        
-      // arra.enumerated() returns a sequence of pairs, where each pair contains the index and the element
-        for (index, square) in Squares.enumerated() {
+        if(playerOneIsPlaying) {
             
-           
-        
-        // Converts the view which the squares and the X symbol relates to, from the parent view of the components (in this case VStack) to the root view of the screen
-            
-            let squareFrame = square.superview?.convert(square.frame, to: view) ?? square.frame
-            
-            let xSymbol = xSymbol.superview?.convert(xSymbol.frame, to: view) ?? xSymbol.frame
-        
-            // Checks if squareFram contains symbol and if the move has not already been done
-            if sender.state == .ended && squareFrame.contains(xSymbol) && square.image != UIImage(systemName: "xmark") &&  square.image != UIImage(systemName: "circle") {
-        
-            
-                square.image = UIImage(systemName: "xmark")
-                
-                square.tintColor = UIColor.systemGreen
-                
-                isPlaying.toggle()
-                lblPlayerOne.text = "Player 1"
-                lblPlayerTwo.text = "Player 2s turn"
-            
-                
-                
-                // Logic to change value of selected square image from 0 to 1 or 2 based on player
-                if game.checkWinner(index: index, player: PLAYER_ONE) {
-                    
-                    resetGame()
-                    
-                } 
-                
-                if(!game.gameBoard.contains(0)) {
-                    resetGame()
-                }
-            
-            }
-            
+            handleDrag(for: xSymbol, sender: sender, player: PLAYER_ONE, playerId: PLAYER_ONE.id, playerOneLbl: lblPlayerOne, playerTwoLbl: lblPlayerTwo, imageName: "xmark", tintcolor: UIColor.systemGreen)
         }
         
-        if sender.state == .ended {
-            print(game.gameBoard)
-            xSymbol.center = initialSymbolPosition
-           
-           
-        }
         
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func onODrag(_ sender: UIPanGestureRecognizer) {
+        
+        if (!playerOneIsPlaying) {
+            
+            handleDrag(for: oSymbol, sender: sender, player: PLAYER_TWO, playerId: PLAYER_TWO.id, playerOneLbl: lblPlayerOne, playerTwoLbl: lblPlayerTwo, imageName: "circle", tintcolor: UIColor.systemPink)
+            
+            
         }
         
         
     }
     
-   
-   
-       
-        
-    @IBAction func onODrag(_ sender: UIPanGestureRecognizer) {
-        
-        if (!isPlaying) {
-            
-            
-            
-           
+    
+    
+    func handleDrag(for symbol: UIImageView, sender: UIPanGestureRecognizer, player: Player, playerId: Int, playerOneLbl: UILabel, playerTwoLbl: UILabel, imageName: String, tintcolor: UIColor) {
         
         let translation = sender.translation(in: self.view)
-         
-        oSymbol.center = CGPoint(x: translation.x + oSymbol.center.x, y: translation.y + oSymbol.center.y )
+        
+        symbol.center = CGPoint(x: translation.x + symbol.center.x, y: translation.y + symbol.center.y )
         
         sender.setTranslation(CGPoint.zero, in: self.view)
         
         for (index, square) in Squares.enumerated() {
-        
-        // Converts the view which the squares and the X symbol relates to, from the parent view of the components (in this case VStack) to the root view of the screen
             
+            // Converts the view which the squares and the symbol relates to, from the parent view of the components (in this case VStack) to the root view of the screen
             let squareFrame = square.superview?.convert(square.frame, to: view) ?? square.frame
+            let symbolFrame = symbol.superview?.convert(symbol.frame, to: view) ?? symbol.frame
             
-            let oSymbol = oSymbol.superview?.convert(oSymbol.frame, to: view) ?? oSymbol.frame
-        
             // Checks if squareFram contains symbol and if the move has not already been done
-            if sender.state == .ended && squareFrame.contains(oSymbol) && square.image != UIImage(systemName: "xmark") &&  square.image != UIImage(systemName: "circle") {
-    
-            
-                square.image = UIImage(systemName: "circle")
-                square.tintColor = UIColor.systemPink
-                isPlaying.toggle()
-                lblPlayerTwo.text = "Player 2"
-                lblPlayerOne.text = "Player 1s turn"
+            if sender.state == .ended && squareFrame.contains(symbolFrame) && square.image != UIImage(systemName: "xmark") &&  square.image != UIImage(systemName: "circle") {
                 
                 
-                if game.checkWinner(index: index, player: PLAYER_TWO) {
-                resetGame()
-
-                } 
+                square.image = UIImage(systemName: imageName)
+                square.tintColor = tintcolor
+                playerOneIsPlaying.toggle()
+                
+                if playerId == PLAYER_ONE.id{
+                    playerOneLbl.text = "Player 1"
+                    playerTwoLbl.text = "Player 2s turn"
+                    
+                } else {
+                    playerTwoLbl.text = "Player 2"
+                    playerOneLbl.text = "Player 1s turn"
+                }
+                
+                if game.checkWinner(index: index, player: player) {
+        
+                    
+                    resetGame()
+                    
+                }
                 if(!game.gameBoard.contains(0)) {
                     resetGame()
                 }
                 
                 print(game.gameBoard)
-
+                
             }
             
         }
         
         if sender.state == .ended {
-            oSymbol.center = initialSymbolPosition
+            symbol.center = initialSymbolPosition
         }
         
-        }
+        
+        
+        
         
     }
     
-
     
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
